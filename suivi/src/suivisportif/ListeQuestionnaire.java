121d775ff75119f10cfcae286e9ad87c258c5c35
mestrequentin <mestre.quentin0@gmail.com>
2018-02-13 14:34:33 +0100

    add Questionnaire

--- suivi/src/suivisportif/ListeQuestionnaire.java
+++ suivi/src/suivisportif/ListeQuestionnaire.java
@@ -1,10 +1,12 @@
 package suivisportif;
 
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListeQuestionnaire {
   
-  ArrayList<Questionnaire> listQ;
+  private ArrayList<Questionnaire> listQ;
 
   public ListeQuestionnaire() {
     super();
@@ -17,5 +19,30 @@ public class ListeQuestionnaire {
 
   public void setListQ(ArrayList<Questionnaire> listQ) {
     this.listQ = listQ;
+  }
+  
+  public void addQuestionnaire(String titre, String sstitre, Date dateD, Date dateF) {
+    Questionnaire Q = new Questionnaire(dateD, dateF);
+    Q.setTitre(titre);
+    Q.setSstitre(sstitre);
+    
+    Scanner sc = new Scanner(System.in);
+    String scanne = "o";
+    String rd;
+    Question q;
+    
+    while(scanne.toUpperCase().equals("O") == true) {
+      System.out.println("Saisissez l'intitulé de la question : ");
+      scanne = sc.nextLine();
+      System.out.println("Réponse par défaut [0 = oui / 1 = non] : ");
+      rd = sc.nextLine();
+      if (Integer.parseInt(rd) == 0) {
+        q = new Question(scanne, true);
+      }
+      else {
+        q = new Question(scanne, false);
+      }
+      Q.addQuestion(q);
+    }
   }
 }
