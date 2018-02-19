354feff03eccec8510204e7b567d2c9b6069cb9c
mestrequentin <mestre.quentin0@gmail.com>
2018-02-19 15:33:06 +0100

    save

--- suivi/src/suivisportif/ListeQuestionnaire.java
+++ suivi/src/suivisportif/ListeQuestionnaire.java
@@ -21,18 +21,31 @@ public class ListeQuestionnaire {
     this.listQ = listQ;
   }
   
-  /**Ajoute un questionnaire.
+  public int testModifQuestionnaire(Questionnaire quest) {
+    Date d = quest.getDateD();
+    Date f = quest.getDateF();
+    Date currD = new Date();
+    
+    if (d.before(currD) && f.after(currD)) {
+      return -1;
+    }
+    if (d.after(currD) || f.before(currD))
+  }
+  
+  /**Ajoute/Crée un questionnaire.
    * 
    * @param titre : titre du question.
    * @param sstitre : sous titre du question.
    * @param dateD : date de début du questionnaire.
    * @param dateF : date de fin du questionnaire.
+   * @return 
    */
   @SuppressWarnings("resource")
-  public void addQuestionnaire(String titre, String sstitre, Date dateD, Date dateF) {
+  public int addQuestionnaire(String titre, String sstitre, String msgFin, Date dateD, Date dateF, Question qulist ...) {
     Questionnaire quest = new Questionnaire(dateD, dateF);
     quest.setTitre(titre);
     quest.setSstitre(sstitre);
+    quest.setMessageFin(msgFin);
     
     Scanner sc = new Scanner(System.in);
     String scanne = "o";
@@ -55,35 +68,56 @@ public class ListeQuestionnaire {
     }
 
     this.listQ.add(quest);
-    return;
+    return 0;
+  }
+  
+  public Object addQuestionnaire(String titre, String sstitre, Date dateD, Date dateF) {
+    return addQuestionnaire( titre, sstitre, null, dateD, dateF);
   }
   
   /**Modifie un Questionnaire.
    * 
    * @param quest : questionnaire a modifier
    */
-  /*
+  @SuppressWarnings({ "deprecation", "unused", "resource" })
   public void modifQuestionnaireDate(Questionnaire quest) {
     Date currD = new Date();
     Date d = quest.getDateD();
     Date f = quest.getDateF();
     Date tmp;
     Scanner sc = new Scanner(System.in);
+    String scanne = new String();
     int j;
     int m;
     int a;
     
     if (currD.after(d) == true && currD.before(f) == true) {
-      
+      System.out.println("Vous ne pouvez modifier que la date de fin!");
+      tmp = f;
+      System.out.println("Le jour de fin :  ");
+      j = sc.nextInt();
+      System.out.println("Le mois de fin :  ");
+      m = sc.nextInt();
+      System.out.println("L'année de fin :  ");
+      a = sc.nextInt() - 1900;
+      f = new Date(a,m,j);
     }
-  }*/
+    else if (currD.after(d) == true && currD.before(f) == true) {
+      System.out.println("Quelle date voulez-vou modifiez ? [d = debut / f = fin]  ");
+      scanne = sc.nextLine();
+    }
+  }
   
+  /** Le main du programme.
+   * 
+   * @param args : argument.
+   */
   @SuppressWarnings("deprecation")
   public static void main(String[] args) {
-    ListeQuestionnaire lQ = new ListeQuestionnaire();
-    lQ.addQuestionnaire("titre","sous titre", new Date(), new Date(2020 - 1900, 3, 8));
-    for(int i = 0; i < lQ.listQ.size(); i++) {
-      System.out.println(lQ.listQ.get(i).toString());
+    ListeQuestionnaire lq = new ListeQuestionnaire();
+    lq.addQuestionnaire("titre","sous titre", new Date(), new Date(2020 - 1900, 3, 8));
+    for (int i = 0; i < lq.listQ.size(); i++) {
+      System.out.println(lq.listQ.get(i).toString());
     }
   }
 }
